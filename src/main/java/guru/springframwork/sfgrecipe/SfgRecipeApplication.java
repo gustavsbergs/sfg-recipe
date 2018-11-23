0a14a63ac6b3bb2fa200d6fd2116727e0ca323f9
telumindel <42297556+telumindel@users.noreply.github.com>
2018-11-23 10:04:07 +0200

    Initial commit

---
+++ src/main/java/guru/springframwork/sfgrecipe/SfgRecipeApplication.java
@@ -0,0 +1,12 @@
+package guru.springframwork.sfgrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipeApplication.class, args);
+	}
+}
