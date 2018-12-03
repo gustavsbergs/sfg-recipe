93aba3bf7de400ccabb75b03c49fc149a2480237
telumindel <42297556+telumindel@users.noreply.github.com>
2018-12-03 10:24:09 +0200

    Service push

--- src/main/java/guru/springframwork/sfgrecipe/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframwork/sfgrecipe/bootstrap/RecipeBootstrap.java
@@ -161,7 +161,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
                  "\n" +
                 "https://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
         Notes tacoNotes = new Notes();
-        tacoNotes.setRecipeNotes("We have a family motto and it is this: Everything goes better in a tortilla.\n" +
+        tacoNotes.setRecipeNotes("We have a family motto and it is this: Everything goes better in a tortilla!\n" +
                 "Any and every kind of leftover can go inside a warm tortilla, usually with a healthy dose of pickled jalapenos. I can always sniff out a late-night snacker when the aroma of tortillas heating in a hot pan on the stove comes wafting through the house.\n"+
                 "Today’s tacos are more purposeful – a deliberate meal instead of a secretive midnight snack!\n" +
                 "First, I marinate the chicken briefly in a spicy paste of ancho chile powder, oregano, cumin, and sweet orange juice while the grill is heating. You can also use this time to prepare the taco toppings.\n" +
